57ca3600ad1b74af07b8247f29b328dca3cd783c
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-21 00:16:33 +0800

    pseudocode

---
+++ exercise_three.py
@@ -0,0 +1,8 @@
+# Print characters from a string that are present at an even index number
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+
+#pseudocode
+#ask user input
+#display the word inputted
+#range of the word
+#printing the result
