8b143e2e1e9ecb4b7e81f4592aae9a09ed995ff3
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-21 00:36:42 +0800

    making the code

--- exercise_three.py
+++ exercise_three.py
@@ -3,6 +3,15 @@
 
 #pseudocode
 #ask user input
+word = input("Type the word here: ")
+
 #display the word inputted
+print("The original word is: ", word)
 #range of the word
+length = len(word)
+
 #printing the result
+('Printing only even index chars')
+
+for i in range(0, length -1, 2):
+    print("[",i,"]", word[i])
